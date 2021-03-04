6ffccbc052f85fc2914c5e793f81a277838de91f
Bailey Sostek <bhsostek@wpi.edu>
2021-03-04 11:05:47 -0500

    Fixed boneInfluence in shader

--- resources/shaders/pbr_vertex.glsl
+++ resources/shaders/pbr_vertex.glsl
@@ -33,7 +33,6 @@ uniform mat4 lightSpaceMatrix[maxLights];
 
 //Bones
 uniform int numBones;
-uniform int boneIndexOffset;
 layout(std430, binding = 0) buffer Bones{
     mat4 boneTransforms[];
 } bonesLocal;
@@ -56,19 +55,23 @@ out vec4[maxLights] passPosLightSpace;
 void main(){
     //reconstruct our IN matricies
     mat4 transform = mat4(
-    transform_0,
-    transform_1,
-    transform_2,
-    transform_3
+        transform_0,
+        transform_1,
+        transform_2,
+        transform_3
     );
 
+    //Create a mat4 we will use for mesh deformation
     mat4 boneTransform;
+    //IF we have bones in this model
     if(numBones > 0){
+        //Calculate the bone transform
         boneTransform =
-        (bonesLocal.boneTransforms[uint(boneIndices.x) + (numBones * gl_InstanceID) + boneIndexOffset] * boneWeights.x) +
-        (bonesLocal.boneTransforms[uint(boneIndices.y) + (numBones * gl_InstanceID) + boneIndexOffset] * boneWeights.y) +
-        (bonesLocal.boneTransforms[uint(boneIndices.z) + (numBones * gl_InstanceID) + boneIndexOffset] * boneWeights.z);
+        (bonesLocal.boneTransforms[uint(boneIndices.x) + (numBones * gl_InstanceID)] * boneWeights.x) + //Bone weight 1
+        (bonesLocal.boneTransforms[uint(boneIndices.y) + (numBones * gl_InstanceID)] * boneWeights.y) + //Bone weight 2
+        (bonesLocal.boneTransforms[uint(boneIndices.z) + (numBones * gl_InstanceID)] * boneWeights.z);  //Bone weight 3
     }else{
+        // Just upload an identiy matrix here for the bone transform so we will have no influence over the model geometry when we dont have an animated model.
         boneTransform = mat4(
             1, 0, 0, 0,
             0, 1, 0, 0,
